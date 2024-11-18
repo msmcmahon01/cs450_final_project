60790c99c72c6d2676ad8b7744c946b2178e7749
Matthew McMahon <msmcmahon01@gmail.com>
2024-11-18 14:14:51 -0800

    Fix N6

--- dots/N6.cpp
+++ dots/N6.cpp
@@ -20,21 +20,21 @@ void loadN6() {
 	N6x.AddTimeValue(0.625, 28);
 
 	N6z.Init();
-	N6z.AddTimeValue(0.0, );
-	N6z.AddTimeValue(10.0, );
-	N6z.AddTimeValue(9.375, );
-	N6z.AddTimeValue(8.75, );
-	N6z.AddTimeValue(8.125, );
-	N6z.AddTimeValue(7.5, );
-	N6z.AddTimeValue(6.875, );
-	N6z.AddTimeValue(6.25, );
-	N6z.AddTimeValue(5.625, );
-	N6z.AddTimeValue(5.0, );
-	N6z.AddTimeValue(4.375, );
-	N6z.AddTimeValue(3.75, );
-	N6z.AddTimeValue(3.125, );
-	N6z.AddTimeValue(2.5, );
-	N6z.AddTimeValue(1.875, );
-	N6z.AddTimeValue(1.25, );
-	N6z.AddTimeValue(0.625, );
+	N6z.AddTimeValue(0.0, -46);
+	N6z.AddTimeValue(10.0, -12);
+	N6z.AddTimeValue(9.375, -12);
+	N6z.AddTimeValue(8.75, -12);
+	N6z.AddTimeValue(8.125, -12);
+	N6z.AddTimeValue(7.5, -12);
+	N6z.AddTimeValue(6.875, -12);
+	N6z.AddTimeValue(6.25, -12);
+	N6z.AddTimeValue(5.625, -12);
+	N6z.AddTimeValue(5.0, -10);
+	N6z.AddTimeValue(4.375, -10);
+	N6z.AddTimeValue(3.75, -6);
+	N6z.AddTimeValue(3.125, -6);
+	N6z.AddTimeValue(2.5, -14);
+	N6z.AddTimeValue(1.875, -14);
+	N6z.AddTimeValue(1.25, -18);
+	N6z.AddTimeValue(0.625, -34);
 }

5d054b2ab870efd071552522bc16f2c54430ab75
Matthew McMahon <msmcmahon01@gmail.com>
2024-12-09 12:47:43 -0800

    Add dots T11 through T15

--- dots/times/times.cpp
+++ dots/times/times.cpp
@@ -8,8 +8,8 @@
 #define t7  52.8
 #define t8  56.0
 #define t9  59.2
-#define t10 65.6    // 65.6
-#define t11 69      // 72
-#define t12 70.4    // 78.4
-#define t13 72.8    // 84.8
-#define t14 74.0
+#define t10 65.6
+#define t11 69
+#define t12 70.4
+#define t13 72.8
+#define t14 73.6

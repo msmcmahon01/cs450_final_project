30f576cec8fab03cad20c883cf8065e59e982ae8
Matthew McMahon <msmcmahon01@gmail.com>
2024-12-08 20:26:13 -0800

    Add dots T30 through T43

--- dots/times/times.cpp
+++ dots/times/times.cpp
@@ -1,6 +1,6 @@
 #define t0  0.0
-#define t1  14.8
-#define t2  25.6
+#define t1  12.8
+#define t2  23.6
 #define t3  34.0
 #define t4  38.4
 #define t5  41.6
@@ -12,4 +12,4 @@
 #define t11 69      // 72
 #define t12 70.4    // 78.4
 #define t13 72.8    // 84.8
-#define t14 73.0
+#define t14 74.0
